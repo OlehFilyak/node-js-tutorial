69b0cded10b6671ad2d74f82879b6942de8f5208
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-09-18 17:05:24 +0300

    add test server

--- index.js
+++ index.js
@@ -13,3 +13,22 @@
 
 //   obj.sayHello()
 // }) ()
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  res.write('<h1>Hello from Node.js</h1>');
+  res.write('<h2>Hello from Node.js</h2>');
+  res.write('<h3>Hello from Node.js</h3>');
+  res.end(`
+<div style="background-color:red; width=100px; height:50px;">
+<h1>Test</h1>
+</div>
+`);
+});
+
+server.listen(3000, () => {
+  console.log('Server is running');
+});
