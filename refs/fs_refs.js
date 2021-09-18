f85bf5138c3b6dcde918d63d08bf75218bc17fe8
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-09-18 13:02:50 +0300

    add fs module

---
+++ refs/fs_refs.js
@@ -0,0 +1,45 @@
+const fs = require('fs'); // File System
+const path = require('path');
+
+// fs.mkdir(path.join(__dirname, 'notes'), (err) => {
+//   if (err) throw err;
+
+//   console.log('Папка була створена');
+// });
+
+// fs.writeFile(
+//   path.join(__dirname, 'notes', 'mynotes.txt'),
+//   'Hello world',
+//   (err) => {
+//     if (err) throw error;
+//     console.log('Файл створений');
+//   }
+// );
+
+// fs.appendFile(
+//   path.join(__dirname, 'notes', 'mynotes.txt'),
+//   ' From append file',
+//   (err) => {
+//     if (err) throw err;
+//     console.log('Файл був змінений');
+//   }
+// );
+
+// fs.readFile(
+//   path.join(__dirname, 'notes', 'mynotes.txt'),
+//   'utf-8', // gjnhs,yj lkz rjhtrnyjuj yfv dslj,hf;tyyz gjdsljvktyyz/
+//   (err, data) => {
+//     if (err) throw err;
+//     console.log(data); // виведе повідомлення з нотаток в консоль
+//   }
+// );
+
+// fs.rename(
+//   path.join(__dirname, 'notes', 'mynotes.txt'),
+//   path.join(__dirname, 'notes', 'notes.txt'),
+//   (err) => {
+//     if (err) throw err;
+
+//     console.log('Файл змінено');
+//   }
+// );
