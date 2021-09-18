a519bf8c680523cea34f9a72675015e553303e70
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-09-18 13:55:03 +0300

    add os module

---
+++ refs/os_ref.js
@@ -0,0 +1,60 @@
+const os = require('os');
+
+// Платформа
+// console.log(os.platform()); // darwin
+
+// Архітектура
+// console.log(os.arch()); //x64
+
+// Інформація
+// console.log(os.cpus());
+// [
+//   {
+//     model: 'Intel(R) Core(TM) i7-7820HQ CPU @ 2.90GHz',
+//     speed: 2900,
+//     times: { user: 1229180, nice: 0, sys: 1095520, idle: 13380580, irq: 0 }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i7-7820HQ CPU @ 2.90GHz',
+//     speed: 2900,
+//     times: { user: 113410, nice: 0, sys: 88100, idle: 15503260, irq: 0 }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i7-7820HQ CPU @ 2.90GHz',
+//     speed: 2900,
+//     times: { user: 1036920, nice: 0, sys: 632740, idle: 14035110, irq: 0 }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i7-7820HQ CPU @ 2.90GHz',
+//     speed: 2900,
+//     times: { user: 109620, nice: 0, sys: 73650, idle: 15521490, irq: 0 }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i7-7820HQ CPU @ 2.90GHz',
+//     speed: 2900,
+//     times: { user: 818990, nice: 0, sys: 437790, idle: 14447990, irq: 0 }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i7-7820HQ CPU @ 2.90GHz',
+//     speed: 2900,
+//     times: { user: 109270, nice: 0, sys: 66990, idle: 15528500, irq: 0 }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i7-7820HQ CPU @ 2.90GHz',
+//     speed: 2900,
+//     times: { user: 719200, nice: 0, sys: 357920, idle: 14627650, irq: 0 }
+//   },
+//   {
+//     model: 'Intel(R) Core(TM) i7-7820HQ CPU @ 2.90GHz',
+//     speed: 2900,
+//     times: { user: 109160, nice: 0, sys: 62080, idle: 15533530, irq: 0 }
+//   }
+
+// Вільна пам'ять
+// console.log(os.freemem()); //122720256
+
+//Скільки всього пам'яті
+// console.log(os.totalmem()); //17179869184;
+
+// Корнева дерикторія
+// console.log(os.homedir()); ///Users/oleh_filak
